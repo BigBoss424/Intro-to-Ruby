31d606ae16ba99845c006e721a28cfc0219f556a
System Administrator <root@markjonessmbp2.attlocal.net>
2014-12-25 13:42:22 -0800

    Created some basic examples of the Ruby language. Covering topics such as whitespace and syntax structure.

---
+++ Examples/whiteSpaceRuby.rb
@@ -0,0 +1,10 @@
+#! /usr/bin/ruby -w 
+
+#This is how we comment in Ruby#
+
+#Example of whitespace and it's uses in Ruby#
+
+# a + b is interpreted as a+b (Here a is a local variable)#
+# a +b is interpreted as a(+b) (Here a is a method call)#
+
+
